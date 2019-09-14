88f57bbd4142792118942898f68599d84a1d25a8
uğur uzun <dangalice-mal@hotmail.com>
2019-09-14 21:50:52 +0300

    Sipariş sepeti tamamlandı.

---
+++ E-TradeProject.MvcWebUI/Models/ShippingDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_TradeProject.MvcWebUI.Models
+{
+    public class ShippingDetails
+    {
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen adres tanımını giriniz.")]
+        [Display(Name = "Adres Başlığı")]
+        public string AdresBasligi { get; set; }
+
+        [Required(ErrorMessage = "Lütfen bir adres giriniz.")]
+        public string Adres { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şehir giriniz.")]
+        [Display(Name = "Şehir")]
+        public string Sehir { get; set; }
+
+        [Required(ErrorMessage = "Lütfen semt giriniz.")]
+        public string Semt { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mahalle giriniz.")]
+        public string Mahalle { get; set; }
+
+        [Display(Name = "Posta Kodu")]
+        public string PostaKodu { get; set; }
+    }
+}
